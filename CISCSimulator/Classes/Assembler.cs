05d057b26dc24b2dea9110f19e9fc74ec10d8fb1
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-14 18:36:44 +0300

    Create Assembler class and use it in MainWindow

---
+++ CISCSimulator/Classes/Assembler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace CISCSimulator
+{ 
+    class Assembler
+    {
+        private readonly Dictionary<string, int> tokens = new Dictionary<string, int>
+        {
+            {"ADD", 0000},
+        };
+
+        private readonly string[] delimiters = { ":", ",", " ", "(", ")" };
+
+        List<string> foundTokens = new List<string>();
+
+        TextFieldParser parser;
+
+
+        public List<string> Parse(string filepath)
+        {
+            int lineCounter = 0;
+
+            parser = new TextFieldParser(filepath)
+            {
+                TextFieldType = FieldType.Delimited,
+                Delimiters = delimiters
+            };
+
+            while (!parser.EndOfData)
+            {
+                string[] lineTokens = parser.ReadFields();
+
+                foreach (string token in lineTokens)
+                {
+                    if (!token.Equals(""))
+                    {
+                        foundTokens.Add(token);
+                    }
+                }
+                lineCounter++;
+            }
+
+            parser.Close();
+
+            if (lineCounter == 0)
+            {
+                Exception exception = new Exception("The ASM file is empty!");
+                throw exception;
+            }
+
+            return foundTokens;
+        }
+
+        public string[] RemoveComments(string filepath)
+        {
+            List<string> codeWithoutComments = new List<string>();
+            string[] commentDelimiters = { ";" };
+            parser = new TextFieldParser(filepath)
+            {
+                TextFieldType = FieldType.Delimited,
+                Delimiters = commentDelimiters
+            };
+
+            while (!parser.EndOfData)
+            {
+                string[] elements = parser.ReadFields();
+
+                foreach (string element in elements)
+                {
+                    if (!element.Equals("") && Array.IndexOf(elements, element) != 1) //Daca are index 1 inseamna ca e dupa ';'
+                    {
+                        codeWithoutComments.Add(element);
+                    }
+                }
+            }
+
+            return codeWithoutComments.ToArray<string>();
+        }
+    }
+}
--- CISCSimulator/MainWindow.xaml.cs
+++ CISCSimulator/MainWindow.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace CISCSimulator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Assembler assembler;
+        string filepath;
+
         public MainWindow()
         {
             InitializeComponent();
+            assembler = new Assembler();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filepath = openFileDialog.FileName;
+                string text = File.ReadAllText(filepath);
+                codeTextBox.Text = text;
+            }
+        }
+
+        private void ParseCode(object sender, RoutedEventArgs e)
+        {
+            tokensListView.Items.Clear();
+            string[] textNoComments = assembler.RemoveComments(filepath);
+            string newPath = string.Format("{0}NoComments.asm", filepath.Remove(filepath.Length - 4, 4));
+            File.WriteAllLines(newPath, textNoComments);
+            List<string> foundTokens = assembler.Parse(newPath);
+
+            foreach (string token in foundTokens)
+            {
+                tokensListView.Items.Add(token);
+            }
         }
     }
 }
